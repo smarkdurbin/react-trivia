328a8da4b4fb1696f69f4e40a1dd31f7eec5d11a
smarkdurbin <smark.durbin@gmail.com>
2022-06-20 17:49:01 -0400

    Removed .archive directory.

--- src/components/.archive/HowToPlay.js
+++
@@ -1,9 +0,0 @@
-const HowToPlay = () => {
-  return (
-    <div className="HowToPlay">
-      <h2>How to Play</h2>
-    </div>
-  );
-};
-
-export default HowToPlay;
--- src/components/.archive/Settings.js
+++
@@ -1,5 +0,0 @@
-const Settings = () => {
-  return <div className="Settings"></div>;
-};
-
-export default Settings;
